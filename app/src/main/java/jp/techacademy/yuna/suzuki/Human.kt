b59959eb86d03085b2fc233bc1b84d5e70503e73
YunaSuzuki <yuna.com.jp@outlook.jp>
2019-12-27 17:15:20 +0900

    修正：humanクラスにのみhobby変数を定義

--- app/src/main/java/jp/techacademy/yuna/suzuki/Human.kt
+++ app/src/main/java/jp/techacademy/yuna/suzuki/Human.kt
@@ -4,8 +4,9 @@ import android.util.Log
 
 open class Human :Animal, Thinkable {
 
+    var hobby: String
 
-    constructor(name: String, age:Int, hobby:String): super(name, age, hobby){
+    constructor(name: String, age:Int, hobby: String): super(name, age){
         this.name = name
         this.age = age
         this.hobby = hobby
